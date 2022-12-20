de6204174eee0bee04ac02556b6b7b9401be3cd9
j-yong98 <chlwodyd9829@gmail.com>
2022-12-20 22:26:26 +0900

    금 채굴 v

--- untitled/src/test/java/Problem4Test.java
+++ untitled/src/test/java/Problem4Test.java
@@ -1,23 +1,40 @@
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class Problem4Test {
 
 
-    @Test
-    void solution() throws IOException {
+    @ParameterizedTest
+    @MethodSource("init")
+    void solution(String p, int ans) throws IOException {
         Problem4 problem4 = new Problem4();
-        String p = "3 2\n" +
-                "0 1 0\n" +
-                "1 0 1\n" +
-                "0 0 0\n";
         System.setIn(new ByteArrayInputStream(p.getBytes()));
         int solution = problem4.solution();
-        Assertions.assertThat(solution).isEqualTo(3);
+        Assertions.assertThat(solution).isEqualTo(ans);
+    }
+    static Stream<Arguments> init(){
+        return Stream.of(
+                arguments("3 1\n" +
+                        "1 0 0\n" +
+                        "0 0 0\n" +
+                        "0 0 1", 1),
+                arguments("5 5\n" +
+                        "0 0 0 0 0\n" +
+                        "0 1 0 0 0\n" +
+                        "0 0 1 0 1\n" +
+                        "0 0 0 0 0\n" +
+                        "0 0 0 1 0\n",3)
+        );
     }
 }
