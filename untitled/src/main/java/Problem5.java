a8846f3a88c1d54272e4b396e93bfbf59287d86c
j-yong98 <chlwodyd9829@gmail.com>
2022-12-20 19:28:24 +0900

    컨베이어 벨트

--- untitled/src/main/java/Problem5.java
+++ untitled/src/main/java/Problem5.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Problem5 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem5 {
+    int n,t;
+    int[] arr;
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+        arr = new int[n*2];
+        st = new StringTokenizer(br.readLine());
+        int i;
+        for (i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int j = 0; j < n; j++){
+            arr[i+j] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int j = 0; j < t; j++){
+            int tmp = arr[n*2-1];
+            for (int k = n*2-1; k >= 1; k--){
+                arr[k] = arr[k-1];
+            }
+            arr[0] = tmp;
+        }
+        for (i = 0; i < n; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        for (int j = 0; j < n; j++){
+            System.out.print(arr[j+i]+" ");
+        }
+    }
 }
--- untitled/src/test/java/Problem5Test.java
+++ untitled/src/test/java/Problem5Test.java
@@ -1,4 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class Problem5Test {
-  
+
+    @Test
+    void solution() throws IOException {
+        Problem5 problem5 = new Problem5();
+        String p = "3 1\n" +
+                "1 2 3\n" +
+                "6 5 1\n";
+        System.setIn(new ByteArrayInputStream(p.getBytes()));
+        problem5.solution();
+    }
+
 }
