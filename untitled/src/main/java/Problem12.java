a05fda8bc4cbaa47b1be3576c81deccc14deb913
j-yong98 <chlwodyd9829@gmail.com>
2022-12-21 19:38:44 +0900

    run_length_encoding

---
+++ untitled/src/main/java/Problem12.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem12 {
+    int n;
+    char[] arr;
+
+    public String encoding(char[] arr){
+        String res = "";
+        int cnt = 1;
+        char pre = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if (pre == arr[i]) cnt++;
+            else{
+                res += pre;
+                res += Integer.toString(cnt);
+                pre = arr[i];
+                cnt = 1;
+            }
+        }
+        res += pre;
+        res += Integer.toString(cnt);
+//        System.out.println(res);
+        return res;
+    }
+
+    private void shiftRight(){
+        char tmp = arr[n-1];
+        for (int i = n - 1; i >= 1; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = tmp;
+    }
+
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String s = st.nextToken();
+        n = s.length();
+        arr= s.toCharArray();
+        int ans = encoding(arr).length();
+        for (int i = 0; i < n; i++){
+            shiftRight();
+            int res = encoding(arr).length();
+            ans = Math.min(ans,res);
+        }
+        System.out.println(ans);
+    }
+}
---
+++ untitled/src/test/java/Problem12Test.java
@@ -0,0 +1,46 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class Problem12Test {
+    Problem12 problem12;
+
+    @BeforeEach
+    void init(){
+        problem12 = new Problem12();
+    }
+    @Test
+    void encodingTest(){
+        String encoding = problem12.encoding("aaabbbbcaa".toCharArray());
+
+        assertThat(encoding).isEqualTo("a3b4c1a2");
+    }
+
+    @ParameterizedTest
+    @MethodSource("problem")
+    void minRunLengthEncodingTest(String p, Integer ans) throws IOException {
+        System.setIn(new ByteArrayInputStream(p.getBytes()));
+        OutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        problem12.solution();
+
+        assertThat(Integer.parseInt(outputStream.toString().trim())).isEqualTo(ans);
+    }
+
+    static Stream<Arguments> problem(){
+        return Stream.of(
+                arguments("aaabbbbcaa",6),
+                arguments("aaaaaaaaaa",3)
+        );
+    }
+}
