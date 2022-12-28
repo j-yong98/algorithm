5d8acf2b5d4742c80ea587fac2192bc7ff39f22f
j-yong98 <chlwodyd9829@gmail.com>
2022-12-28 20:59:08 +0900

    검증 추가

--- java/src/test/java/others/Problem44Test.java
+++ java/src/test/java/others/Problem44Test.java
@@ -1,14 +1,23 @@
 package others;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Problem44Test {
+    OutputStream outputStream;
+    Problem44 problem44;
 
+    @BeforeEach
+    void init(){
+        problem44 = new Problem44();
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
     @Test
     void solution() throws IOException {
         Problem44 problem44 = new Problem44();
@@ -19,6 +28,11 @@ class Problem44Test {
                 "14 6 9 10\n";
         System.setIn(new ByteArrayInputStream(p.getBytes()));
         problem44.solution();
+        Assertions.assertThat(outputStream.toString().trim()).isEqualTo(
+                "13 4 1 9 \n" +
+                "12 15 7 11 \n" +
+                "14 2 5 16 \n" +
+                "6 8 3 10");
     }
 
 }
--- java/src/test/java/others/Problem45Test.java
+++ java/src/test/java/others/Problem45Test.java
@@ -1,23 +1,41 @@
 package others;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Problem45Test {
+    OutputStream outputStream;
+    Problem45 problem45;
+
+    @BeforeEach
+    void init(){
+        outputStream = new ByteArrayOutputStream();
+        problem45 = new Problem45();
+        System.setOut(new PrintStream(outputStream));
+    }
 
     @Test
     void solution() throws IOException {
-        Problem45 problem45 = new Problem45();
-        String p ="1\n" +
+        String p = "3\n" +
+                "2 2\n" +
+                "1 1 L\n" +
+                "2 2 R\n" +
                 "2 2\n" +
                 "1 1 D\n" +
-                "2 2 R\n";
+                "2 2 R\n" +
+                "2 2\n" +
+                "1 1 L\n" +
+                "1 2 R\n";
         System.setIn(new ByteArrayInputStream(p.getBytes()));
         problem45.solution();
+        Assertions.assertThat(outputStream.toString().trim()).isEqualTo("2\n" +
+                "0\n" +
+                "2");
     }
 
 }
