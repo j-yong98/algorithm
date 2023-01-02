5e03199413f8e0b36f9ea377ea2e59cfb3621dc4
j-yong98 <chlwodyd9829@gmail.com>
2023-01-02 22:22:22 +0900

    수들의 합 최대화 하기

--- java/src/main/java/others/Problem65.java
+++ java/src/main/java/others/Problem65.java
@@ -3,44 +3,55 @@ package others;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Problem65 {
     int n;
     int[][] arr;
-    int[][] visited;
+    int[] visited;
+
+    List<Integer> list = new ArrayList<>();
     int ans = 0;
 
-    private void f(int l,int sum){
+    private void f(int l){
         if (l == n){
-            ans = Math.max(ans,sum);
+            int res = 0;
+            for (int i = 0; i < n; i++){
+                res += arr[i][list.get(i)];
+            }
+            ans = Math.max(ans,res);
             return;
         }
-        for (int i = l; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if (visited[0][i] == 0 && visited[1][j] == 0){
-                    visited[0][i] = 1;
-                    visited[1][j] = 1;
-                    f(l+1,sum + arr[i][j]);
-                    visited[0][i] = 0;
-                    visited[1][j] = 0;
-                }
+        for (int i = 0; i < n; i++){
+            if (visited[i] == 0) {
+                visited[i] = 1;
+                list.add(i);
+                f(l+1);
+                visited[i] = 0;
+                list.remove(list.size()-1);
             }
         }
     }
+
+    /**
+     * 각 행과 열에 정확히 한 개의 색칠 칸만 오도록 이런 합들 중 최댓값은?
+     * @throws IOException
+     */
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         arr = new int[n][n];
-        visited = new int[2][n];
+        visited = new int[n];
         for (int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        f(0,0);
+        f(0);
         System.out.println(ans);
     }
 }
