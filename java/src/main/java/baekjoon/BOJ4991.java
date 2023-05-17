d1a7d0ed2bcb793c1c2a70f56738e4b61cfe6346
j-yong98 <chlwodyd9829@gmail.com>
2023-05-18 00:52:14 +0900

    로봇 청소기

--- java/src/main/java/baekjoon/BOJ4991.java
+++ java/src/main/java/baekjoon/BOJ4991.java
@@ -1,110 +1,111 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class BOJ4991 {
-    final char clean = '.';
-    final char duty = '*';
-    final char furniture = 'x';
-    int r, c;
-    int[] pos = new int[3];
-    int[] dy = {-1, 1, 0, 0};
-    int[] dx = {0, 0, 1, -1};
-    int[] orders;
-    char[][] arr = new char[21][21];
-    List<int[]> dusts = new ArrayList<>();
-    boolean[] checked;
+    final char ROBOT = 'o';
+    final char DUST = '*';
+    final char OBSTACLE = 'x';
+
+    int w, h;
+    int size = 0;
     int ans;
+    char[][] arr;
+    int[][] dist;
+    List<Position> dusts = new ArrayList<>();
 
-    private boolean inRange(int y, int x) {
-        return y >= 0 && y < r && x >= 0 && x < c;
-    }
-
-    private int bfs() {
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[r][c];
-        visited[pos[0]][pos[1]] = true;
-        q.add(new int[]{pos[0], pos[1], 0});
-        int sum = 0;
-        int idx = 0;
-        while (!q.isEmpty()) {
-            int[] p = q.pollFirst();
-            if (p[0] == dusts.get(orders[idx])[0] && p[1] == dusts.get(orders[idx])[1]) {
-                sum += p[2];
-                q.clear();
-                for (int i = 0; i < r; i++) {
-                    for (int j = 0; j < c; j++)
-                        visited[i][j] = false;
-                }
-                visited[p[0]][p[1]] = true;
-                q.addLast(new int[]{p[0], p[1], 0});
-                idx++;
-                if (idx == dusts.size())
-                    break ;
-                continue;
-            }
-            for (int i = 0; i < 4; i++) {
-                int yy = p[0] + dy[i];
-                int xx = p[1] + dx[i];
-                if (inRange(yy, xx) && !visited[yy][xx] && arr[yy][xx] != furniture) {
-                    visited[yy][xx] = true;
-                    q.addLast(new int[]{yy, xx, p[2] + 1});
-                }
-            }
-        }
-        if (idx != dusts.size())
-            return Integer.MAX_VALUE;
-        return sum == 0 ? Integer.MAX_VALUE : sum;
-    }
-
-    private void comb(int depth) {
-        if (depth == dusts.size()) {
-            ans = Math.min(ans, bfs());
-            return;
-        }
-        for (int i = 0; i < dusts.size(); i++) {
-            if (!checked[i]) {
-                orders[depth] = i;
-                checked[i] = true;
-                comb(depth + 1);
-                checked[i] = false;
-            }
-        }
-    }
-
-    /**
-     * 로봇 청소기
-     */
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         while (true) {
             String[] line = br.readLine().split(" ");
-            c = Integer.parseInt(line[0]);
-            r = Integer.parseInt(line[1]);
-            ans = Integer.MAX_VALUE;
+            w = Integer.parseInt(line[0]);
+            h = Integer.parseInt(line[1]);
+            if (w == 0 && h == 0)
+                break ;
+            arr = new char[h][w];
+
             dusts.clear();
-            if (r == 0 && c == 0)
-                break;
-            for (int i = 0; i < r; i++) {
-                String row = br.readLine();
-                for (int j = 0; j < c; j++) {
-                    arr[i][j] = row.charAt(j);
-                    if (arr[i][j] == 'o') {
-                        pos[0] = i;
-                        pos[1] = j;
-                    } else if (arr[i][j] == duty)
-                        dusts.add(new int[]{i, j});
+            for (int i = 0; i < h; i++) {
+                String l = br.readLine();
+                for (int j = 0; j < w; j++) {
+                    arr[i][j] = l.charAt(j);
+                    if (arr[i][j] == DUST)
+                        dusts.add(new Position(i, j));
+                    else if (arr[i][j] == ROBOT)
+                        dusts.add(0, new Position(i, j));
                 }
             }
-            orders = new int[dusts.size()];
-            checked = new boolean[dusts.size()];
-            comb(0);
-            System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
+
+            ans = Integer.MAX_VALUE;
+            size = dusts.size();
+            dist = new int[size][size];
+            for (int i = 0; i < size; i++) {
+                for (int j = i + 1; j < size; j++) {
+                    dist[i][j] = dist[j][i] = bfs(dusts.get(i), dusts.get(j));
+                }
+            }
+            perm(1, 0, 1, 0);
+            ans = ans == Integer.MAX_VALUE ? -1 : ans;
+            bw.write(ans + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private int bfs(Position s, Position e) {
+        final int[] dy = {-1, 1, 0, 0};
+        final int[] dx = {0, 0, -1, 1};
+
+        Deque<int[]> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[h][w];
+        visited[s.y][s.x] = true;
+        q.add(new int[]{s.y, s.x, 0});
+
+        while (!q.isEmpty()) {
+            int[] pos = q.pollFirst();
+            if (pos[0] == e.y && pos[1] == e.x)
+                return pos[2];
+            for (int i = 0; i < 4; i++) {
+                int y = pos[0] + dy[i];
+                int x = pos[1] + dx[i];
+                if (inRange(y, x) && !visited[y][x] && arr[y][x] != OBSTACLE) {
+                    visited[y][x] = true;
+                    q.add(new int[]{y, x, pos[2] + 1});
+                }
+            }
+        }
+        return -1;
+    }
+
+    private void perm(int l, int from, int visited, int sum) {
+        if (l == size) {
+            ans = Math.min(ans, sum);
+            return ;
+        }
+        for (int i = 0; i < size; i++) {
+            if (dist[from][i] == -1) continue;
+            if ((visited & (1 << i)) == 0)
+                perm(l + 1, i, visited | (1 << i), sum + dist[from][i]);
+        }
+    }
+
+    private boolean inRange(int y, int x) {
+        return y >= 0 && y < h && x >= 0 && x < w;
+    }
+
+    class Position {
+        int y;
+        int x;
+
+        public Position(int y, int x) {
+            this.y = y;
+            this.x = x;
         }
     }
 }
