fb08f7166493052f4b3fd803be91cf44a57a4aae
j-yong98 <chlwodyd9829@gmail.com>
2023-05-23 18:04:42 +0900

    모노미노도미노

--- java/src/main/java/baekjoon/BOJ19235.java
+++ java/src/main/java/baekjoon/BOJ19235.java
@@ -8,10 +8,7 @@ public class BOJ19235 {
     final int[][][] BLOCKS = {
             null, {{0}, {0}}, {{0, 0}, {0, 1}}, {{0, 1}, {0, 0}}
     };
-    int[][] blue = new int[SIZE][SIZE + 2];
-    int[][] green = new int[SIZE + 2][SIZE];
-    int n;
-    int ans = 0;
+    int[][][] arr = new int[2][SIZE + 2][SIZE];
 
     /**
      * 모노미노도미노
@@ -19,225 +16,142 @@ public class BOJ19235 {
     public void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int bomb = 0;
         for (int i = 0; i < n; i++) {
             String[] line = br.readLine().split(" ");
             int t = Integer.parseInt(line[0]);
             int y = Integer.parseInt(line[1]);
             int x = Integer.parseInt(line[2]);
-            putBlock(t, y, x);
-            ans += bomb();
-            checkArea();
-        }
-        printMap();
-        int cnt = 0;
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE + 2; j++) {
-                if (blue[i][j] != BLANK)
-                    cnt++;
+            putBlock(arr[0], BLOCKS[t], t, x); //green
+            bomb += removeBlock(arr[0]);
+            checkArea(arr[0]);
+            if (t == 2 || t == 3) {
+                t = t == 2 ? 3 : 2;
             }
+            putBlock(arr[1], BLOCKS[t], t, y);
+            bomb += removeBlock(arr[1]);
+            checkArea(arr[1]);
         }
+        int cnt = 0;
         for (int i = 0; i < SIZE + 2; i++) {
             for (int j = 0; j < SIZE; j++) {
-                if (green[i][j] != BLANK)
+                if (arr[0][i][j] != BLANK)
+                    cnt++;
+                if (arr[1][i][j] != BLANK)
                     cnt++;
             }
         }
-        bw.write(ans + "\n" + cnt);
+        bw.write(bomb + "\n" + cnt);
         bw.flush();
         bw.close();
         br.close();
     }
 
-    private void printMap() {
-        System.out.println("blue");
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE + 2; j++)
-                System.out.print(blue[i][j] + " ");
-            System.out.println();
-        }
-        System.out.println("green");
+    private void printBoard(int[][] board, int y, int x, int type) {
+        System.out.println("y : " + y + " " + "x : " + x + " type: " + type);
         for (int i = 0; i < SIZE + 2; i++) {
             for (int j = 0; j < SIZE; j++)
-                System.out.print(green[i][j] + " ");
+                System.out.print(board[i][j] + " ");
             System.out.println();
         }
-        System.out.println("========================");
+        System.out.println("=======================");
     }
 
-    private void putBlock(int t, int y, int x) {
-        int[][] block = BLOCKS[t];
+    private void putBlock(int[][] board, int[][] block, int type, int x) {
         int tmp = 0;
         for (int i = 1; i < SIZE + 2; i++) {
-            boolean can = true;
-            for (int d = 0; d < block[0].length; d++) {
-                int yy = y + block[0][d];
-                int xx = i + block[1][d];
-                if (inRange(yy, xx, SIZE, SIZE + 2) && blue[yy][xx] == BLANK)
-                    continue;
-                can = false;
-                break;
-            }
-            if (!can)
-                break;
-            tmp = i;
-        }
-        for (int d = 0; d < block[0].length; d++) {
-            int yy = y + block[0][d];
-            int xx = tmp + block[1][d];
-            blue[yy][xx] = t;
-        }
-        tmp = 0;
-        for (int i = 1; i < SIZE + 2; i++) {
-            boolean can = true;
+            boolean flag = true;
             for (int d = 0; d < block[0].length; d++) {
                 int yy = i + block[0][d];
                 int xx = x + block[1][d];
-                if (inRange(yy, xx, SIZE + 2, SIZE) && green[yy][xx] == BLANK)
-                    continue;
-                can = false;
+                if (inRange(yy, xx) && board[yy][xx] == BLANK) continue;
+                flag = false;
                 break;
             }
-            if (!can)
+            if (!flag)
                 break;
             tmp = i;
         }
         for (int d = 0; d < block[0].length; d++) {
             int yy = tmp + block[0][d];
             int xx = x + block[1][d];
-            green[yy][xx] = t;
+            board[yy][xx] = type == 2 ? 2 : 1;
         }
     }
 
-    private int bomb() {
-        int res1 = 0;
-        for (int i = 2; i < SIZE + 2; i++) {
+    private int removeBlock(int[][] board) {
+        int res = 0;
+        for (int i = 0; i < SIZE + 2; i++) {
             int cnt = 0;
             for (int j = 0; j < SIZE; j++) {
-                if (blue[j][i] != BLANK)
+                if (board[i][j] != BLANK)
                     cnt += 1;
             }
-            if (cnt == SIZE) {
-                res1 += 1;
-                for (int j = 0; j < SIZE; j++)
-                    blue[j][i] = BLANK;
-                fallDown(blue);
-            }
-        }
-        int res2 = 0;
-        for (int i = 2; i < SIZE + 2; i++) {
-            int cnt = 0;
+
+            if (cnt != SIZE) continue;
+
+            res += 1;
             for (int j = 0; j < SIZE; j++) {
-                if (green[i][j] != BLANK)
-                    cnt++;
+                board[i][j] = BLANK;
             }
-            if (cnt == SIZE) {
-                res2 += 1;
-                for (int j = 0; j < SIZE; j++)
-                    green[i][j] = BLANK;
-                fallDown(green);
+            int[][] tmp = fallDown(board);
+            for (int r = 0; r < SIZE + 2; r++) {
+                for (int c = 0; c < SIZE; c++)
+                    board[r][c] = tmp[r][c];
             }
+            break ;
         }
-        if (res1 + res2 == 0)
-            return 0;
-        return res1 + res2 + bomb();
+        if (res > 0)
+            return removeBlock(board) + 1;
+        return 0;
     }
 
-    private void fallDown(int[][] board) {
-        if (board == blue) {
-            int[][] tmp = new int[SIZE][SIZE + 2];
-            for (int i = 0; i < SIZE; i++) {
-                int idx = SIZE + 2 - 1;
-                for (int j = SIZE + 2 - 1; j >= 0; j--) {
-                    if (blue[i][j] == BLANK)
-                        continue;
-                    if (blue[i][j] == 3) {
-                        while (!(tmp[i][idx] == BLANK && tmp[i + 1][idx] == BLANK))
-                            idx--;
-                        tmp[i][idx] = 2;
-                        tmp[i + 1][idx] = 2;
-                        idx--;
-                        blue[i + 1][j] = BLANK;
-                    }
-                    else {
-                        while (tmp[i][idx] != BLANK)
-                            idx--;
-                        tmp[i][idx--] = blue[i][j];
-                    }
+    private int[][] fallDown(int[][] board) {
+        int[][] tmp = new int[SIZE + 2][SIZE];
+        for (int i = SIZE + 2 - 1; i >= 0; i--) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == BLANK) continue;
+                int[][] block = BLOCKS[board[i][j]];
+                putBlock(tmp, block, board[i][j], j);
+                for (int d = 0; d < block[0].length; d++) {
+                    int y = i + block[0][d];
+                    int x = j + block[1][d];
+                    board[y][x] = BLANK;
                 }
             }
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE + 2; j++)
-                    blue[i][j] = tmp[i][j];
-            }
-        } else {
-            int[][] tmp = new int[SIZE + 2][SIZE];
-            for (int i = 0; i < SIZE; i++) {
-                int idx = SIZE + 2 - 1;
-                for (int j = SIZE + 2 - 1; j >= 0; j--) {
-                    if (green[j][i] == BLANK)
-                        continue;
-                    if (green[j][i] == 2) {
-                        while (!(tmp[idx][i] == BLANK && tmp[idx][i + 1] == BLANK))
-                            idx--;
-                        tmp[idx][i] = 2;
-                        tmp[idx][i + 1] = 2;
-                        idx--;
-                        green[j][i + 1] = 0;
-                    }
-                    else {
-                        while (tmp[idx][i] != BLANK)
-                            idx--;
-                        tmp[idx--][i] = green[j][i];
-                    }
-                }
-            }
-            for (int i = 0; i < SIZE + 2; i++) {
-                for (int j = 0; j < SIZE; j++)
-                    green[i][j] = tmp[i][j];
-            }
+        }
+        return tmp;
+    }
+
+    private void checkArea(int[][] board) {
+        int tmp = 0;
+        for (int i = 0; i < SIZE; i++) {
+            if (board[1][i] == BLANK)
+                continue;
+            tmp++;
+            break;
+        }
+        for (int i = 0; i < SIZE; i++) {
+            if (board[0][i] == BLANK)
+                continue;
+            tmp++;
+            break;
+        }
+        if (tmp == 0)
+            return;
+
+        for (int i = SIZE + 2 - 1; i >= tmp; i--) {
+            for (int j = 0; j < SIZE; j++)
+                board[i][j] = board[i - tmp][j];
+        }
+        for (int i = 0; i < SIZE; i++) {
+            board[0][i] = BLANK;
+            board[1][i] = BLANK;
         }
     }
 
-    private void checkArea() {
-        boolean flag = false;
-        for (int i = 0; i < SIZE; i++) {
-            if (blue[i][1] == BLANK)
-                continue;
-            flag = true;
-            break;
-        }
-        if (flag) {
-            for (int i = SIZE + 2 - 1; i > 0; i--) {
-                for (int j = 0; j < SIZE; j++)
-                    blue[j][i] = blue[j][i - 1];
-            }
-            for (int i = 0; i < SIZE; i++) {
-                blue[i][0] = BLANK;
-                blue[i][1] = BLANK;
-            }
-        }
-
-        flag = false;
-        for (int i = 0; i < SIZE; i++) {
-            if (green[1][i] == BLANK)
-                continue;
-            flag = true;
-            break;
-        }
-        if (flag) {
-            for (int i = SIZE + 2 - 1; i > 0; i--) {
-                for (int j = 0; j < SIZE; j++)
-                    green[i][j] = green[i - 1][j];
-            }
-            for (int i = 0; i < SIZE; i++) {
-                green[0][i] = BLANK;
-                green[1][i] = BLANK;
-            }
-        }
-    }
-
-    private boolean inRange(int y, int x, int r, int c) {
-        return y >= 0 && y < r && x >= 0 && x < c;
+    private boolean inRange(int y, int x) {
+        return y >= 0 && y < SIZE + 2 && x >= 0 && x < SIZE;
     }
 }
--- java/src/test/java/baekjoon/BOJ19235Test.java
+++ java/src/test/java/baekjoon/BOJ19235Test.java
@@ -1,6 +1,7 @@
 package baekjoon;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,13 +12,18 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class BOJ19235Test {
+    OutputStream out;
+
+    @BeforeEach
+    void init() {
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
     @ParameterizedTest
     @MethodSource("testCase")
     void solution(String p, String ans) throws IOException {
-        OutputStream out = new ByteArrayOutputStream();
         BOJ19235 boj19235 = new BOJ19235();
         System.setIn(new ByteArrayInputStream(p.getBytes()));
-        System.setOut(new PrintStream(out));
         boj19235.solution();
         String res = out.toString();
         Assertions.assertThat(res).isEqualTo(ans);
@@ -89,6 +95,25 @@ class BOJ19235Test {
                         "1 0 0\n" +
                         "2 0 1\n" +
                         "3 0 3", "1\n15"),
+                arguments("18\n" +
+                        "1 2 2\n" +
+                        "1 2 3\n" +
+                        "2 0 0\n" +
+                        "1 2 0\n" +
+                        "1 1 2\n" +
+                        "1 1 0\n" +
+                        "2 3 0\n" +
+                        "3 0 1\n" +
+                        "3 1 3\n" +
+                        "2 1 0\n" +
+                        "1 2 0\n" +
+                        "2 3 0\n" +
+                        "2 2 1\n" +
+                        "1 2 2\n" +
+                        "3 0 3\n" +
+                        "1 2 0\n" +
+                        "2 2 0\n" +
+                        "3 2 3", "6\n10"),
                 arguments("25\n" +
                         "3 1 0\n" +
                         "2 2 2\n" +
@@ -114,7 +139,34 @@ class BOJ19235Test {
                         "2 0 2\n" +
                         "1 0 0\n" +
                         "2 2 1\n" +
-                        "1 0 1", "3\n11")
+                        "1 0 1", "3\n11"),
+                arguments("5\n" +
+                        "1 3 3\n" +
+                        "2 2 2\n" +
+                        "2 1 1\n" +
+                        "2 0 0\n" +
+                        "2 0 2", "2\n10"),
+                arguments("6\n" +
+                        "2 3 0\n" +
+                        "1 3 2\n" +
+                        "1 2 1\n" +
+                        "2 2 2\n" +
+                        "2 1 0\n" +
+                        "2 1 2", "1\n16"),
+                arguments("6\n" +
+                        "1 1 0\n" +
+                        "2 1 0\n" +
+                        "3 1 2\n" +
+                        "3 0 1\n" +
+                        "3 0 2\n" +
+                        "3 0 3", "2\n11"),
+                arguments("6\n" +
+                        "1 0 0\n" +
+                        "1 0 2\n" +
+                        "2 0 0\n" +
+                        "3 0 2\n" +
+                        "2 0 1\n" +
+                        "3 0 3", "1\n12")
         );
     }
 }
