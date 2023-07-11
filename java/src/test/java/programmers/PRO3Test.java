757a8a8dee36bef0ed1c344f41d5d14f611ad369
j-yong98 <chlwodyd9829@gmail.com>
2023-07-11 21:20:38 +0900

    이모티콘 할인행사

---
+++ java/src/test/java/programmers/PRO3Test.java
@@ -0,0 +1,11 @@
+package programmers;
+
+import org.junit.jupiter.api.Test;
+
+class PRO3Test {
+    @Test
+    void solution() {
+        PRO3 pro3 = new PRO3();
+        pro3.solution(new int[][]{{40, 10000}, {25, 10000}}, new int[]{7000, 9000});
+    }
+}
