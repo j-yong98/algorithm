9b019613302fa6e11598aaaa74c1215063295a32
j-yong98 <chlwodyd9829@gmail.com>
2023-09-09 20:29:00 +0900

    별자리 만들기 테케 추가

--- java/src/test/java/baekjoon/BOJ4386Test.java
+++ java/src/test/java/baekjoon/BOJ4386Test.java
@@ -3,13 +3,17 @@ package baekjoon;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 class BOJ4386Test {
     @Test
-    void solution() {
+    void solution() throws IOException {
         BOJ4386 boj4386 = new BOJ4386();
-        String p = "";
+        String p = "3\n" +
+                "1.0 1.0\n" +
+                "2.0 2.0\n" +
+                "2.0 4.0";
         System.setIn(new ByteArrayInputStream(p.getBytes()));
-        boj4386.so
+        boj4386.solution();
     }
 }
